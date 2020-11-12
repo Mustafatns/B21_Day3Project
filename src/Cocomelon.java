8cd562d0582f9c9ed14e7d63967657056e5b909e
Mustafa Tanis <mustafatanis97@gmail.com>
2020-11-12 11:33:46 -0500

    1- Still working on it

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,5 +1,10 @@
 public class Cocomelon {
     public static void main(String[] args) {
+     int[] scores = {10,20,50,70,80};
+
+     for (int each : scores){
+         System.out.println(each);
+     }
 
     }
 }
