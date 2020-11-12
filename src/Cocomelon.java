69417485ce6b0dc864d40ca65113ec2320312eea
Mustafa Tanis <mustafatanis97@gmail.com>
2020-11-12 11:54:23 -0500

    Still adding more stuff

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class Cocomelon {
     public static void main(String[] args) {
      int[] scores = {10,20,50,70,80};
 
      for (int each : scores){
          System.out.println(each);
+         System.out.println(Arrays.toString(scores));
      }
 
     }
